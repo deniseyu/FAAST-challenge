71d6d23be0adeec577b40daaf76e003b37d9fcd5
Denise <yu.denise.d@gmail.com>
2014-09-21 18:19:53 +0100

    Changed around default wallet balance code

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,13 +1,19 @@
 class Passenger
 
-	attr_accessor :passengers
+	attr_accessor :passengers, :wallet_balance
+
+	DEFAULT_BALANCE = 20
+
+	def initialize(option = {})
+		@wallet_balance = option.fetch(:wallet_balance, DEFAULT_BALANCE)
+	end
 
 	def enter(station)
 		@passengers << passenger
 	end
 
 	def wallet_balance
-		@wallet_balance ||= 20
+		@wallet_balance
 	end
 
 	def tap_in
@@ -17,5 +23,11 @@ class Passenger
 	def top_up(amount)
 		@wallet_balance += amount 
 	end
+
+	# def paid?
+	# 	passenger.tap_in = true 
+	# end
+	# Here, I considered writing a method to help the station validate 
+	# that the passenger paid.. but I don't know how just yet
 
 end
--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -23,25 +23,4 @@ describe Passenger do
 		expect(passenger.wallet_balance).to eq 30
 	end
 
-
-	# it "should be able to pay for a journey at the station" do 
-	# 	expect(passenger.wallet_balance).to_not eq 0
-	# 	passenger.pay
-	# 	expect()
-
-
-	# it "should be touch in at a station" do 
-	# 	passenger = Passenger.new
-	# 	# expect(station).to receive(:passenger_count).and_return 0
-	# 	station.passenger_count
-	# 	expect(station.passenger_count).to eq 0
-	# 	passenger.touch_in(station)
-	# 	# allow(station).to receive(:admit).with(passenger)
-	# 	# station.admit(passenger)
-	# 	# expect(station).to receive(:passenger_count).and_return 1
-	# 	# station.passenger_count
-	# end
-
-	# it "should be able to leave a station"
-
 end
